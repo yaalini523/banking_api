6480fc5c1366cf219eae02c62bd9678050d40f83
yaalini523 <sekaryaalini@gmail.com>
2025-08-08 10:55:29 +0000

    Update logic and restructure files

--- Frontend/src/components/RegisterForm.js
+++ Frontend/src/components/RegisterForm.js
@@ -1,26 +1,31 @@
-import React, { useState } from "react";
-import './RegisterForm.css';
+import React, { Component } from "react";
+import "./RegisterForm.css";
 import axios from "axios";
 
-const RegisterForm = () => {
-  const [form, setForm] = useState({
-    first_name: "",
-    last_name: "",
-    dob: "",
-    address: "",
-    phone_number: "",
-    email: "",
-    aadhaar_number: ""
-  });
+class RegisterForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      first_name: "",
+      last_name: "",
+      dob: "",
+      address: "",
+      phone_number: "",
+      email: "",
+      aadhaar_number: ""
+    };
+  }
 
-  const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
+  // reading the values -> name: value
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async (e) => {
+  handleSubmit = async (e) => {
+    // prevents from refreshing the page after submit
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/register", form);
+      const res = await axios.post("http://localhost:5000/register", this.state);
 
       if (res.data && res.data.message) {
         alert(res.data.message);
@@ -37,21 +42,58 @@ const RegisterForm = () => {
     }
   };
 
-  return (
-    <div className="register-container">
-      <h2>Register Account Holder</h2>
-      <form onSubmit={handleSubmit} className="register-form">
-        <input name="first_name" placeholder="First Name" onChange={handleChange} required />
-        <input name="last_name" placeholder="Last Name" onChange={handleChange} required />
-        <input type="date" name="dob" onChange={handleChange} required />
-        <input name="address" placeholder="Address" onChange={handleChange} required />
-        <input name="phone_number" placeholder="Phone Number" onChange={handleChange} required />
-        <input name="email" placeholder="Email" onChange={handleChange} required />
-        <input name="aadhaar_number" placeholder="Aadhaar Number" onChange={handleChange} required />
-        <button type="submit">Register</button>
-      </form>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="register-container">
+        <h2>Register Account Holder</h2>
+        <form onSubmit={this.handleSubmit} className="register-form">
+          <input
+            name="first_name"
+            placeholder="First Name"
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            name="last_name"
+            placeholder="Last Name"
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            type="date"
+            name="dob"
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            name="address"
+            placeholder="Address"
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            name="phone_number"
+            placeholder="Phone Number"
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            name="email"
+            placeholder="Email"
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            name="aadhaar_number"
+            placeholder="Aadhaar Number"
+            onChange={this.handleChange}
+            required
+          />
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default RegisterForm;
